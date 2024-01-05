8ae26da070195ae6dcadfdce1ac141547caab4cd
Tyler Olson <tydotg@gmail.com>
2024-01-04 19:38:17 -0800

    add server build

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Based on Linear architecture (see: [Scaling the Linear Sync Engine](https://www.
 Design doc: https://docs.google.com/document/d/1bRGZz7YL1sd_YNg3AiyfbPUT2cLwgcOtFyp1dcpaqGI/edit?usp=sharing
 
 TODO...
+
+### commands
+
+```sh
+$ npm run app:build
+$ npm run app:watch
+$ npm run server:build
+$ npm run server:watch
+```
--- webpack/app.js
+++ webpack/app.js
@@ -1,28 +1,19 @@
 const path = require('path'); // eslint-disable-line @typescript-eslint/no-var-requires
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/app/index.tsx'),
+  entry: path.resolve(__dirname, '../src/app/index.tsx'),
   mode: 'development',
   target: 'web',
   devtool: 'inline-source-map',
   output: {
     filename: 'app.bundle.js',
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../dist'),
     publicPath: '/dist',
   },
   module: { 
     rules: [{
       test: /\.css$/i,
       use: ["style-loader", "css-loader"],
-    }, {
-      test: /\.tsx?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'ts-loader',
-        options: {
-          onlyCompileBundledFiles: true,
-        }
-      },
     }] 
   },
   resolve: {
