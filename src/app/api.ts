513a7794d36295692b012a96d69dba247797c029
Tyler Olson <tydotg@gmail.com>
2024-01-08 22:42:56 -0800

    quick hack to finalllyyy fully use graphql

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,22 +1,81 @@
 import { TodoItem } from "@prisma/client";
 import { CreateTodoItem } from "../shared/types";
 
-export const fetchTodos = (): Promise<TodoItem[]> => fetch('/api/todos').then(res => res.json());
-
-export const createTodo = (partialTodo: CreateTodoItem): Promise<TodoItem> => 
-  fetch('/api/todos', {
+export const fetchTodos = (): Promise<TodoItem[]> => 
+  fetch('/api/graphql', {
     method: 'POST', 
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(partialTodo)
-  }).then(res => res.json());
+    body: JSON.stringify({
+      query: `
+        query {
+          todoItems(orderBy: {createdAt: asc}) {
+            id
+            text
+            complete
+            hidden
+            createdAt
+            updatedAt
+          }
+        }
+      `
+    })
+  })
+    .then(res => res.json())
+    .then(json => json.data.todoItems);
 
+export const createTodo = (partialTodo: CreateTodoItem): Promise<TodoItem> => 
+  fetch('/api/graphql', {
+    method: 'POST', 
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        mutation {
+          createOneTodoItem(data: {
+            text: "${partialTodo.text}",
+            complete: ${partialTodo.complete},
+            hidden: ${partialTodo.hidden}
+          }) {
+            id
+            text
+            complete
+            hidden
+            createdAt
+            updatedAt
+          }
+        }
+      `
+    })
+  })
+    .then(res => res.json())
+    .then(res => res.data.createOneTodoItem);
+
+// Note: hardcoded to only work with updating the `complete` field for now...
 export const updateTodo = (id: number, partialTodo: Partial<TodoItem>): Promise<TodoItem> =>
-  fetch(`/api/todos/${id}`, {
+  fetch('/api/graphql', {
     method: 'POST',
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(partialTodo)
-  }).then(res => res.json());
+    body: JSON.stringify({
+      query: `
+        mutation {
+          updateOneTodoItem(where: { id: ${id} }, data: {
+            complete: {set: ${partialTodo.complete}},
+          }) {
+            id
+            text
+            complete
+            hidden
+            createdAt
+            updatedAt
+          }
+        }
+      `
+    })
+  })
+    .then(res => res.json())
+    .then(res => res.data.updateOneTodoItem);
