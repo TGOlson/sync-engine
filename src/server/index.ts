4412e3da9c1f1d0d53cff8ae28880646b42db78a
Tyler Olson <tydotg@gmail.com>
2024-01-09 23:19:41 -0800

    remove schema

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,5 @@
-import path from "path";
-import { writeFile } from "fs/promises";
-import { printSchema } from "graphql";
-
-import { generateSchema, startServer } from "./server";
+import { startServer } from "./server";
 import { generateSeedData } from "./seed-data";
-
 
 const main = async () => {
   const [_, __, cmd] = process.argv;
@@ -12,12 +7,12 @@ const main = async () => {
   switch (cmd) {
     case undefined: return await startServer();
     case "--generate-seed-data": return await generateSeedData();
-    case "--generate-schema": {
-      const schema = await generateSchema();
-      const p = path.resolve(__dirname, '../src/server/__generated__/schema.graphql');
+    // case "--generate-schema": {
+    //   const schema = await generateSchema();
+    //   const p = path.resolve(__dirname, '../src/server/__generated__/schema.graphql');
 
-      return await writeFile(p, printSchema(schema));
-    }
+    //   return await writeFile(p, printSchema(schema));
+    // }
     default:
       console.log("Unknown server command.");
   }
